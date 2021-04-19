aab497936b420e21bea0fba7cfe2cc39cc19fc9b
Ayham <ayhamhn33@gmail.com>
2021-04-19 15:44:00 +0300

    setup sanctum package

--- laravel/app/Models/Post.php
+++ laravel/app/Models/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function usersReations(){
+
+        return $this->belongsToMany(user::class, Reaction::class);
+
+    }// end of usersReations that return who user is reacting post
 }
--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -6,5 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+
+    public function getGenderAttribute($value){
+
+        if ($value == 1){
+
+            return 'male';
+
+        }else if($value == 0){
+
+            return 'female';
+
+        }else {
+
+            return 'Null';
+
+        }
+
+    }
+    public function experiences(){
+
+        return $this->hasMany(Experience::class);
+
+    }// end of experiences relation
+
+    public function educations(){
+
+        return $this->hasMany(Education::class);
+
+    }// end of educations relation
+
+    public function reactions(){
+
+        return $this->belongsToMany(Post::class, Reaction::class);
+
+    }// end of reactions relation
 }
